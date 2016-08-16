9ed84dd75b983e1342762b4b27652bb5138d2088
zyxep <zyxep.net@gmail.com>
2016-08-16 13:04:54 +0200

    Removed submodule rabbitMQ to add the files

---
+++ rabbitmq/update.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+cd "$(dirname "$(readlink -f "$BASH_SOURCE")")"
+
+debianVersion="$(curl -sSL 'http://www.rabbitmq.com/debian/dists/testing/main/binary-amd64/Packages' | grep -m1 -A10 '^Package: rabbitmq-server$' | grep -m1 '^Version: ' | cut -d' ' -f2)"
+
+rabbitmqVersion="${debianVersion%%-*}"
+
+set -x
+sed -ri 's/^(ENV RABBITMQ_VERSION) .*/\1 '"$rabbitmqVersion"'/' Dockerfile
+sed -ri 's/^(ENV RABBITMQ_DEBIAN_VERSION) .*/\1 '"$debianVersion"'/' Dockerfile
